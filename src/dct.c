e62b32e5b4b479e704ee80a702c004f626d422bd
Jamie St Martin <jamiestm9@hotmail.com>
2020-06-15 22:41:32 -0700

    Implemented function which actually runs dct
    main() runs dct(), which takes the image from main, and passes it
    through run_dct() four times. run_dct() takes the image data and a
    function pointer, and runs the passed function on each 8x8 block of the
    image, printing out the results. The four functions that are executed on
    each 8x8 block of the image are:
    
    - A function which copies the input directly to the output. This is used
      only so that the input is printed for comparison purposes.
    
    - A naive implementation of dct, using quadruply-nested for loops and a
      cos(x*pi/16) lookup table. This is used as reference, as it is likely
      the most bug-free version of the three actual implementations (though
      there is no guarantee that it is actually working correctly).
    
    - A simple version of dct using matrix multiplications. This is
      somewhat more efficient than the truly naive version, but is more
      prone to errors. I wasn't able to get the efficient
      matrix-multiplication version working, but it shouldn't be too hard,
      though there isn't much point to it.
    
    - A more complicated version of dct using the Loeffler method. This is
      the most efficient 1-D dct implementation we know about, so we're
      using it. However, this currently seems to be broken, even though as
      far as I can tell, all of the operations are performed correctly as
      according to the diagram and details presented on the slides. The
      issues may relate to either the fact that floating-point operations
      are used, or that the algorithm is incorrectly implemented at a higher
      level than the 1-D implementation (i.e. it may be that the 1-D
      implementation is working correctly but is being called incorrectly).
    
    Additionally, all of these implementations are fundamentally unoptimized
    and (from the perspective of this course) unusable, because they use
    double-precision floating-point arithmetic for all of their operations.
    However, the interfaces to these functions use signed 8-bit integers, so
    we would only need to fix the internals in order to fix them. Frankly, I
    didn't pay enough attention in the class where we covered how to
    maximize precision with trigonometric functions, so I'm not sure how to
    do it (though we don't actually need trigonometric functions - only
    constant values that trigonometric outputs - so we may be able to get
    away with not learning that stuff). This is a critical problem that will
    need to be addressed before starting to generate and optimize assembly.
    
    Another issue is whether the inputs & outputs should be signed or
    unsigned. Online info seems to be unclear. It would seem that unsigned
    inputs could result in negative outputs, which may lead to problems.
    Perhaps internally we could use signed values, then output unsigned
    values if we know that the results will be unsigned - but I'm not
    confident that that would be the case.
    
    A final improvement (which may fix the issue with the Loeffler method,
    but that's not clear) will be to create a Butterfly() routine, as
    directed by the assignment slides, rather than running it directly
    within the Loeffler 1-D dct function. This will also allow us to more
    clearly optimize that routine out into an assembly instruction.

--- src/dct.c
+++ src/dct.c
@@ -1,9 +1,71 @@
 #include "dct.h"
 
+#include <stdio.h>
+
+#include "dct/naive.h"
+#include "dct/twostep_slow.h"
+#include "dct/loeffler.h"
+
+void print_line(const int8_t x[8])
+{
+    printf("%02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx\n",
+            x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]);
+}
+
+void run_dct(const int8_t *data, long width, long height, void (*dct_func)(const int8_t[8][8], int8_t[8][8]) )
+{
+    long width_strides = width / 8;
+    long height_strides = height / 8;
+    for (int i = 0; i < height_strides; ++i) {
+        for (int j = 0; j < width_strides; ++j) {
+            int8_t image_region[8][8];
+            int8_t dct_output_region[8][8];
+            // copy image into tmp
+            for (int k = 0; k < 8; ++k) {
+                for (int l = 0; l < 8; ++l) {
+                    int index = ((i * 8) + k) * width + ((j * 8) + l);
+                    image_region[k][l] = data[index];
+                }
+            }
+            // Call dct function
+            (*dct_func)(image_region, dct_output_region);
+            // print it out
+            printf("Region %d:\n", i * j);
+            for (int l = 0; l < 8; ++l) {
+                print_line(dct_output_region[l]);
+            }
+        }
+    }
+}
+
+// Do-nothing function that just copies input to output. This is only useful as a way
+// of using run_dct to print out the input in the same way it prints out the output.
+void copy_data(const int8_t data_in[8][8], int8_t data_out[8][8])
+{
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            data_out[i][j] = data_in[i][j];
+        }
+    }
+}
 
 void dct(int8_t *data, long width, long height)
 {
     if (width % 8 != 0 || height % 8 != 0) return;
 
-    // TODO: implement
+    // Print input (by using copy_data as the dct function parameter to run_dct)
+    printf("Input:\n");
+    run_dct(data, width, height, copy_data);
+
+    // First: naive implementation
+    printf("\nNaive implementation:\n");
+    run_dct(data, width, height, dct_naive);
+
+    // Second: twostep implementation
+    printf("\nTwo-step implementation:\n");
+    run_dct(data, width, height, dct_twostep_slow);
+
+    // Third: loeffler implementation
+    printf("\nLoeffler implementation:\n");
+    run_dct(data, width, height, dct_loeffler);
 }
