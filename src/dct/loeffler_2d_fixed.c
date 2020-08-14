985fb3afdc09b4a8ad02fcb561437c64594b8484
Jamie St Martin <jamiestm9@hotmail.com>
2020-08-14 08:42:48 -0700

    Removed temporary array copy in 2d loeffler

--- src/dct/loeffler_2d_fixed.c
+++ src/dct/loeffler_2d_fixed.c
@@ -5,7 +5,7 @@
 // compute_t: Type used for computation.
 typedef int16_t compute_t;
 
-// requires 1 extra bit.
+// requires 1 extra bit. requires DCT_PRECISION extra bits to be available.
 static void butterfly(compute_t top, compute_t bot, compute_t *top_out, compute_t *bot_out, uint8_t type)
 {
     compute_t tmp_sum;
@@ -29,7 +29,7 @@ static void butterfly(compute_t top, compute_t bot, compute_t *top_out, compute_
     }
 }
 
-void dct_2d_fixed(compute_t data[8][8])
+void dct_2d_fixed(DataType data_in[8][8], compute_t data[8][8])
 {
     compute_t temp_value; // extra temporary value
     uint8_t i;
@@ -39,14 +39,14 @@ void dct_2d_fixed(compute_t data[8][8])
         // values are 7-bit + 1 sign bit
 
         // STAGE 1
-        temp_value = data[i][0] + data[i][7]; // actually out[0]
-        data[i][7] = data[i][0] - data[i][7]; // actually out[7]
-        data[i][0] = data[i][1] + data[i][6]; // actually out[1]
-        data[i][6] = data[i][1] - data[i][6]; // actually out[6]
-        data[i][1] = data[i][2] + data[i][5]; // actually out[2]
-        data[i][5] = data[i][2] - data[i][5]; // actually out[5]
-        data[i][2] = data[i][3] + data[i][4]; // actually out[3]
-        data[i][4] = data[i][3] - data[i][4]; // actually out[4]
+        temp_value = data_in[i][0] + data_in[i][7]; // actually out[0]
+        data[i][7] = data_in[i][0] - data_in[i][7]; // actually out[7]
+        data[i][0] = data_in[i][1] + data_in[i][6]; // actually out[1]
+        data[i][6] = data_in[i][1] - data_in[i][6]; // actually out[6]
+        data[i][1] = data_in[i][2] + data_in[i][5]; // actually out[2]
+        data[i][5] = data_in[i][2] - data_in[i][5]; // actually out[5]
+        data[i][2] = data_in[i][3] + data_in[i][4]; // actually out[3]
+        data[i][4] = data_in[i][3] - data_in[i][4]; // actually out[4]
         // values are 8-bit + 1 sign bit
 
         // STAGE 2
@@ -131,15 +131,15 @@ void dct_2d_fixed(compute_t data[8][8])
         // these values are 13 bit + 1 sign bit
 
         // STAGE 4
-        data[5][i] = DCT_RT2 * data[5][i] + DCT_RT2_ROUND_VAL >> DCT_RT2_PRECISION; // x[6] -> X[5]
-        data[6][i] = temp_value; // x[3] -> X[6]
-        temp_value = data[7][i] + data[4][i]; // x[7] -> X[1] (store as temp due to dependencies)
-        data[7][i] = data[7][i] - data[4][i]; // x[4] -> X[7]
-        data[4][i] = data[3][i]; // x[1] -> X[4]
-        data[3][i] = DCT_RT2 * data[2][i] + DCT_RT2_ROUND_VAL >> DCT_RT2_PRECISION; // x[5] -> X[3]
-        data[2][i] = data[0][i]; // x[2] -> X[2]
-        data[0][i] = data[1][i]; // x[0] -> X[0]
-        data[1][i] = temp_value; // restore from temp
+        data[5][i] = ((DCT_RT2 * data[5][i] + DCT_RT2_ROUND_VAL >> DCT_RT2_PRECISION) + (1<<2)) >> 3; // x[6] -> X[5]
+        data[6][i] = (temp_value + (1<<2)) >> 3; // x[3] -> X[6]
+        temp_value = (data[7][i] + data[4][i] + (1<<2)) >> 3; // x[7] -> X[1] (store as temp due to dependencies)
+        data[7][i] = (data[7][i] - data[4][i] + (1<<2)) >> 3; // x[4] -> X[7]
+        data[4][i] = (data[3][i] + (1<<2)) >> 3; // x[1] -> X[4]
+        data[3][i] = ((DCT_RT2 * data[2][i] + DCT_RT2_ROUND_VAL >> DCT_RT2_PRECISION) + (1<<2)) >> 3; // x[5] -> X[3]
+        data[2][i] = (data[0][i] + (1<<2)) >> 3; // x[2] -> X[2]
+        data[0][i] = (data[1][i] + (1<<2)) >> 3; // x[0] -> X[0]
+        data[1][i] = (temp_value + (1<<2)) >> 3; // restore from temp
         // these values are at most 13 bit + 1 sign bit.
         // dividing by 8 results in 10 bit + 1 sign bit. So our results clearly fit into 16 bits, but the interim values need to
         // be massaged into such a small representation.
@@ -148,22 +148,7 @@ void dct_2d_fixed(compute_t data[8][8])
 
 void dct_loeffler_2d_fixed(DataType data_in[8][8], int16_t data_out[8][8])
 {
-    int i, j;
-    // [X] = [C] * [x] * [C]^T
-    compute_t tmp[8][8];
-    for (i = 0; i < 8; ++i) {
-        for (j = 0; j < 8; ++j) {
-            tmp[i][j] = data_in[i][j];
-        }
-    }
-
-    dct_2d_fixed(tmp);
-
-    for (i = 0; i < 8; ++i) {
-        for (j = 0; j < 8; ++j) {
-            data_out[i][j] = (int16_t) ((tmp[i][j] + (1<<2)) >> 3); // round to nearest and divide by 8 to account for C(u) factors
-        }
-    }
+    dct_2d_fixed(data_in, data_out);
 }
 
 // DONE level-off
@@ -171,12 +156,12 @@ void dct_loeffler_2d_fixed(DataType data_in[8][8], int16_t data_out[8][8])
 // DONE 7 bit scale factors - otherwise we run out of bits within 16. this is done, but needs some fixing.
 // DONE correct rounding before shifting (add to smallest shifted-out bit before shifting)
 // finish fixing 16 bit interim values
-// remove temporary array copy
+// DONE remove temporary array copy
 // fix right-shift issue: whether right-shift is arithmetic (keeping negative values negative) or logical (filling with zeroes)
 // is implementation-defined for signed inputs. consequently, our shift operations produce undefined behaviour. we'll need to check
 // with the arm-linux-gcc compiler to determine which is used.
 //
-// merge various branches into master to combine these things
+// DONE merge various branches into master to combine these things
 // DONE (install recent git & cmake on seng440.ece.uvic.ca)
 // DONE compile assembly with arm-gcc compiler: arm-linux-gcc -static -o file.exe file.c (or -s for assembly, I think)
 // look at generated assembly with different optimization levels with -O0 -O1 -O2 -O3
